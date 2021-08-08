eb2d5f49572b70e5ad67bed530acdfeac2214cc2
Paul <paul-hildisch@gmx.de>
2021-08-08 23:40:46 +0200

    added candycane

---
+++ src/main/java/net/paulfuenftausend/edibleOres/EdibleOres.java
@@ -0,0 +1,14 @@
+package net.paulfuenftausend.edibleOres;
+
+import net.fabricmc.api.ModInitializer;
+import net.paulfuenftausend.edibleOres.registry.ModItems;
+
+public class EdibleOres implements ModInitializer {
+
+	public static final String MOD_ID =  "ores";
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerItems();
+	}
+}
---
+++ src/main/java/net/paulfuenftausend/edibleOres/registry/ModItems.java
@@ -0,0 +1,16 @@
+package net.paulfuenftausend.edibleOres.registry;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.paulfuenftausend.edibleOres.EdibleOres;
+
+public class ModItems {
+
+    public static final Item CANDYCANE = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+    public static void registerItems(){
+        Registry.register(Registry.ITEM, new Identifier(EdibleOres.MOD_ID, "candycane"), CANDYCANE);
+    }
+}
